85c99064c0a083223756697a88cfc571950fa34d
lanaeleven <maulanaelvn@gmail.com>
2025-05-28 06:09:24 +0800

    -

--- config/kode.php
+++ config/kode.php
@@ -12,8 +12,21 @@ return [
     'rs' => [
         1 => [
             'nama' => 'RSI Sultan Agung Banjarbaru',
-            'urlHasil' => 'hasil.com',
-            'urlRajal' => 'rajal.com'
+            'pelayananIgdRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSdXWQeKS2OhsWR49OOKHtjcAn6fPSn7s1aheC1QenwEKzUnfg/viewform?usp=header',
+            'pelayananIgdRs1b' => 'https://docs.google.com/forms/d/e/1FAIpQLSf9uWrk10M1oFDCzMSLXNKJlr4sT3PEzKLBxq144MYZv8uZkg/viewform?usp=header',
+            'pelayananIgdRs1c' => 'https://docs.google.com/forms/d/e/1FAIpQLSeY6ePWpQsYNqOPzDsLY2r-ysd3Jj8ku_AiIARZtQwRJjLw7Q/viewform?usp=header',
+            'pelayananIgdRs1d' => 'https://docs.google.com/forms/d/e/1FAIpQLSf8zeoHGoMOnVNAEZiyXqKYfx8cIyDSwF256-B-T9VbWL8KRQ/viewform?usp=header',
+            'rawatJalanRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSc-xAVOzCk7u0IgWvsn0l25Z5JkimTgfyQYzQPKnrEjVIpbUg/viewform?usp=header',
+            'rawatJalanRs1b' => 'https://docs.google.com/forms/d/e/1FAIpQLSfDZmsUaUQZiHFa0nQhPS3kjCtNuaAKQXMwumkdGdC9EeQbOg/viewform?usp=header',
+            'p3Rs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSeVHlgNYldIBggcyIj-UGvlNWX57qQ1MA8Z6UqzPrfQk9vnow/viewform?usp=header',
+            'clPasienDgnP3Rs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSefAZKvuatA7CY9CaJr0CXCqtmGv_QFBBh28fDDhqwRRkV_mQ/viewform?usp=header',
+            'formBRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSemf5McGq1Fxx36zjlZSkrRTauV9kRC7hRX546gJ_YiuZ1tBw/viewform?usp=header',
+            'formARs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSfeG0xpCU5MWtRnnA0CVU0rUe3jXtjMUcn3Mr5QUBCGyjvCHg/viewform?usp=header',
+            'catatanLainRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLScanYLe8uq5VnzjOU5_DVMWn9r87RsjKOTkxZe_lHmEc9pk0Q/viewform?usp=header',
+            'kepuasanPelangganRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSch3eGw89zyw8-gJFlk1o5O-YZawqeoqwvcM4Lva8MkgyYihw/viewform?usp=header',
+            'billingRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSeiyNzlrzoCC1DMtUYj36Ld0Ej9zTa2Co4Q5r7yV1p_YViLBQ/viewform?usp=header',
+            'sarprasRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSehsEmQ9KP9WYtLv2kRkASDeNRtlRuIfTQROtBoDYv94OwLBg/viewform?usp=header',
+            'distKetenagaanRs1' => 'https://docs.google.com/forms/d/e/1FAIpQLSe3cfc4aeViJXykkn3RI98NMxw-hLPEhtIxir2FMW2KCK6y1Q/viewform?usp=header',
         ],
         2 => [
             'nama' => 'RS B',

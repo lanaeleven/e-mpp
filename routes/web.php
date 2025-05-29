8a2b36c9c0b19b5ea9bc91e706fa4b45249d76af
lanaeleven <maulanaelvn@gmail.com>
2025-05-29 11:52:44 +0800

    feat: :sparkles: set url static for avoid error in prod

--- routes/web.php
+++ routes/web.php
@@ -169,6 +169,60 @@ Route::get('/tutorial', function () {
     return file_get_contents(public_path('tutorial/index.html'));
 });
 
+Route::get('/menus/oncall/', function () {
+    return file_get_contents(public_path('menus/oncall/index.html'));
+});
+
+Route::get('/primaryMenus/slrs/', function () {
+    return file_get_contents(public_path('primaryMenus/slrs/index.html'));
+});
+
+Route::get('/menus/irj/', function () {
+    return file_get_contents(public_path('menus/irj/index.html'));
+});
+
+Route::get('/menus/igd/', function () {
+    return file_get_contents(public_path('menus/igd/index.html'));
+});
+
+Route::get('/menus/rwi/', function () {
+    return file_get_contents(public_path('menus/rwi/index.html'));
+});
+
+Route::get('/menus/ibs/', function () {
+    return file_get_contents(public_path('menus/ibs/index.html'));
+});
+
+Route::get('/menus/hd/', function () {
+    return file_get_contents(public_path('menus/hd/index.html'));
+});
+
+Route::get('/menus/tppri/', function () {
+    return file_get_contents(public_path('menus/tppri/index.html'));
+});
+
+Route::get('/primaryMenus/ssdm/', function () {
+    return file_get_contents(public_path('primaryMenus/ssdm/index.html'));
+});
+
+Route::get('/primaryMenus/ssarpras/', function () {
+    return file_get_contents(public_path('primaryMenus/ssarpras/index.html'));
+});
+
+Route::get('/primaryMenus/sbilling/', function () {
+    return file_get_contents(public_path('primaryMenus/sbilling/index.html'));
+});
+
+Route::get('/primaryMenus/kepuasan/', function () {
+    return file_get_contents(public_path('primaryMenus/kepuasan/index.html'));
+});
+
+Route::get('/primaryMenus/lainnya/', function () {
+    return file_get_contents(public_path('primaryMenus/lainnya/index.html'));
+});
+
+
+
 
 // Menangani file statis Next.js langsung dari public/
 Route::get('/{path}', function ($path) {
